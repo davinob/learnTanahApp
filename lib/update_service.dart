d91eb792f9f480615c7b8708bd01986e966bbce2
David Boni <DAVIDB+amdocs@amdocs.com>
2026-05-11 10:00:09 +0300

    Bump admin tap-burst from 5 to 10 (within 3s) to avoid accidental activation

--- lib/update_service.dart
+++ lib/update_service.dart
@@ -29,7 +29,7 @@ class UpdateService {
   bool get hasLocalContent => _hasLocalContent;
   String? get localHtmlPath => _localHtmlPath;
 
-  static const int _manifestVersion = 16;
+  static const int _manifestVersion = 17;
 
   Future<void> initialize() async {
     try {
