61e91ad6af97330cc6ff67b072612a316b01cf19
David Boni <DAVIDB+amdocs@amdocs.com>
2026-07-24 02:38:36 +0300

    Target Android 16 (API 36) to meet Google Play requirement.
    Pin compileSdk and targetSdk to 36 (was inheriting Flutter defaults, which Play saw as API 30) and bump version to 2.1.4+4318 for the update.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "learn.nach"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 36
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
@@ -21,7 +21,7 @@ android {
     defaultConfig {
         applicationId = "learn.nach"
         minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
+        targetSdk = 36
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
